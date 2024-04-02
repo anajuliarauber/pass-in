1538e6562945ec095ec03efeaa5b87580939f5ce
anajuliarauber <anajuliarauber@hotmail.com>
2024-04-02 16:27:44 -0300

    feat: add get event

---
+++ src/routes/get-event.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+export async function getEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get("/events/:eventId", {
+    schema: {
+      params: z.object({
+        eventId: z.string().uuid()
+      }),
+      response: {
+        200: {
+          event: z.object({
+            id: z.string().uuid(),
+            title: z.string(),
+            details: z.string().nullable(),
+            slug: z.string(),
+            maximumAttendees: z.number().int().nullable(),
+            attendeesCount: z.number().int()
+          })
+        }
+      }
+
+    }
+  }, async (request, reply) => {
+    const { eventId } = request.params
+
+    const event = await prisma.event.findUnique({
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        details: true,
+        maximumAttendees: true,
+        _count: true
+      },
+      where: {
+        id: eventId
+      }
+    })
+
+    if (event === null) {
+      throw new Error("Event not found.")
+    }
+
+    return reply.status(200).send({
+      event: {
+        id: event.id,
+        title: event.title,
+        slug: event.slug,
+        details: event.details,
+        maximumAttendees: event.maximumAttendees,
+        attendeesCount: event._count.attendees
+      }
+    })
+  })
+
+}
