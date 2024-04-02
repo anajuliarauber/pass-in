73e8c139c2402625194008cfa0ff0c81aba185e5
anajuliarauber <anajuliarauber@hotmail.com>
2024-04-01 21:14:17 -0300

    chore: start project

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get("/",() => {
+  return "Hello nlw"
+})
+
+app.listen({port: 3333}).then(() => {console.log("Server running on port :3333")})
